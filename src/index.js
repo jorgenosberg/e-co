e0a62a736376d844c144d0a54888555da6d2de3d
jorgenosberg <jorgen.osberg@gmail.com>
2022-11-22 20:21:37 -0500

    Removed boilerplate MuiThemeProvider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <MuiThemeProvider>
       <App />
-  </MuiThemeProvider>
 );
