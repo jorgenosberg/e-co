184933adbc30f5d95665a331cde6acf016512a2e
jorgenosberg <jorgen.osberg@gmail.com>
2022-11-28 13:30:37 -0500

    Checks hour if past midnight

--- src/api/stats/index.js
+++ src/api/stats/index.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchDayPrices = async (country) => {
     try {
-        const date = new Date().toLocaleDateString()
+        const date = new Date().toLocaleDateString("en-CA");
+        console.log(country);
+        console.log(date);
         const response = await axios.get(`https://api.iea.org/rte/price/hourly/${country}/timeseries?from=${date}&to=${date}&currency=local`);
         return { values: response.data.map(a => a.Value), labels: response.data.map((a, i) => `${i}:00`) }
     } catch (e) {
@@ -11,15 +13,15 @@ export const fetchDayPrices = async (country) => {
 }
 
 export const fetchWeekPrices = async (country) => {
-    const start = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toLocaleDateString()
-    const end = new Date().toLocaleDateString();
+    const start = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toLocaleDateString("en-CA")
+    const end = new Date().toLocaleDateString("en-CA");
     const response = await axios.get(`https://api.iea.org/rte/price/${country}/timeseries?from=${start}&to=${end}&currency=local&precision=day`);
     return { values: response.data.map(a => a.Value), labels: response.data.map(a => a.Date.slice(5, 10).replace('-', '/')) }
 }
 
 export const fetchMonthPrices = async (country) => {
-    const start = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toLocaleDateString()
-    const end = new Date().toLocaleDateString();
+    const start = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toLocaleDateString("en-CA")
+    const end = new Date().toLocaleDateString("en-CA");
     const response = await axios.get(`https://api.iea.org/rte/price/${country}/timeseries?from=${start}&to=${end}&currency=local&precision=day`);
     const data = response.data.filter((a, i) => i % 2)
     return { values: data.map(a => a.Value), labels: data.map(a => a.Date.slice(5, 10).replace('-', '/')) }
