e0371d186b3eee8f57715c3f738f3961a8b1f124
jorgenosberg <jorgen.osberg@gmail.com>
2022-11-28 11:53:07 -0500

    Removed conflicting changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { faker } from "@faker-js/faker";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -8,45 +7,8 @@ import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { ReactComponent as HomeDecoration } from "../assets/home-decoration.svg";
-import { getAuth } from "firebase/auth";
-import axios from "axios";
 
 function Home() {
-  const [currentPrice, setCurrentPrice] = React.useState(115.4);
-  const [currentEmissions, setCurrentEmissions] = React.useState(62.7);
-
-  const auth = getAuth();
-  const user = auth.currentUser;
-
-  const getUserCountry = async () => {
-    if (user !== null) {
-      const cca2Code = user.country.code;
-      const countryData = await axios.get(`https://restcountries.com/v3.1/alpha/${cca2Code}?fields=cca3`);
-      return countryData.data.cca3;
-    } else {
-      return "FRA";
-    }
-  }
-
-  const getCurrentPrice = async () => {
-    let country = getUserCountry();
-    let today = new Date();
-
-    if (today.getHours() < 8) {
-      today.setDate(today.getDate() - 1);
-      today.setHours(23);
-    }
-    
-    let todayFormatted = today.toLocaleDateString("en-CA");
-    let currentHour = today.getHours();
-    let apiUrl = `https://api.iea.org/rte/price/hourly/${country}/timeseries?from=${todayFormatted}&to=${todayFormatted}&currency=local`;
-
-    const apiData = await axios.get(apiUrl);
-    const pricePerMWh = apiData.data[currentHour].Value;
-    const pricePerKWh = pricePerMWh / 1000;
-
-    return pricePerKWh;
-  }
 
   return (
     <Container>
