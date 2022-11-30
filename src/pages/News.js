9d41305ede45b9ec53d3bb05a11ee2ddeae38617
jorgenosberg <jorgen.osberg@gmail.com>
2022-11-30 11:21:37 -0500

    Fixed grid layout for news page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import NewspaperIcon from "@mui/icons-material/Newspaper";
@@ -26,99 +25,141 @@ function News() {
   const [feed, setFeed] = React.useState("energy");
 
   return (
-    <Box
+    <Grid
+      container
+      spacing={2}
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          width: "55%",
-          height: "85%",
-        }}
-      >
-        <Paper elevation={10} sx={{ py: 4, height: "15%", borderRadius: 10, display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <Avatar
-            sx={{
-              width: "70px",
-              height: "70px",
-              backgroundColor: "secondary.main",
-            }}
-          >
-            <NewspaperIcon sx={{ fontSize: "45px" }} />
-          </Avatar>
-
-          <Typography variant="h4" align="center" sx={{ pl: 4 }}>
-            Energy News
-          </Typography>
-        </Paper>
-
-        <Paper elevation={10} sx={{ px: 2, pt: 4, height: "60%", borderRadius: 10 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={11}>
-              <Tabs
-                value={feed}
-                onChange={(event, newValue) => setFeed(newValue)}
+      <Grid item xs={12} lg={8}>
+        <Paper
+          elevation={10}
+          sx={{
+            p: 5,
+            borderRadius: 10,
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+              <Avatar
+                sx={{
+                  width: "50px",
+                  height: "50px",
+                  backgroundColor: "secondary.main",
+                }}
               >
-                <Tab label="Energy" value="energy" />
-                <Tab label="Emissions" value="emissions" />
-                <Tab label="Sustainability" value="sustainability" />
-              </Tabs>
+                <NewspaperIcon sx={{ fontSize: "35px" }} />
+              </Avatar>
             </Grid>
-            <Grid item xs={1}>
-              <Box display="inline" justifyContent="right">
-                <Tooltip
-                  title="On this page we have compiled a little selection of news about different topics related to energy and sustainability. Please select a topic from the dropdown to access the feed."
-                  placement="right-start"
-                >
-                  <IconButton>
-                    <HelpOutlineIcon sx={{ color: "secondary.main" }} />
-                  </IconButton>
-                </Tooltip>
-              </Box>
-            </Grid>
+
             <Grid item xs={12}>
-              <iframe
-                title="newsfeed"
-                src={feedSources[feed]}
-                height="303px"
-                width="100%"
-                class="fw-iframe"
-                scrolling="no"
-                frameborder="0"
-              ></iframe>
+              <Typography variant="h4" align="center">
+                Energy News
+              </Typography>
+            </Grid>
+
+            <Grid item container spacing={2}>
+              <Grid item xs={11}>
+                <Tabs
+                  value={feed}
+                  onChange={(event, newValue) => setFeed(newValue)}
+                >
+                  <Tab label="Energy" value="energy" />
+                  <Tab label="Emissions" value="emissions" />
+                  <Tab label="Sustainability" value="sustainability" />
+                </Tabs>
+              </Grid>
+              <Grid item xs={1}>
+                <Box display="inline" justifyContent="right">
+                  <Tooltip
+                    title="On this page we have compiled a curated selection of news articles from reputable sources. We have selected these articles based on their relevance to the energy sector and their quality. We hope you enjoy reading them!"
+                    placement="right-start"
+                  >
+                    <IconButton>
+                      <HelpOutlineIcon sx={{ color: "secondary.main" }} />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <iframe
+                  title="newsfeed"
+                  src={feedSources[feed]}
+                  height="303px"
+                  width="100%"
+                  class="fw-iframe"
+                  scrolling="no"
+                  frameborder="0"
+                ></iframe>
+              </Grid>
+            </Grid>
+
+            <Grid item container xs={12}>
+              <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                  Our recommended news outlets at the moment:
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    alignItems: "center",
+                  }}
+                >
+                  <a href="https://www.ft.com/">
+                    <img
+                      src={FTLogo}
+                      alt="Financial Times"
+                      style={{ width: "80px" }}
+                    />
+                  </a>
+                  <a href="https://www.euronews.com/">
+                    <img
+                      src={ENLogo}
+                      alt="Euronews"
+                      style={{ width: "150px" }}
+                    />
+                  </a>
+                  <a href="https://energypost.eu/">
+                    <img
+                      src={EPLogo}
+                      alt="Energy Post"
+                      style={{ width: "150px" }}
+                    />
+                  </a>
+                </Box>
+              </Grid>
             </Grid>
           </Grid>
         </Paper>
-
-        <Paper elevation={10} sx={{ pt: 1, height: "20%", borderRadius: 10 }}>
-          <Typography variant="h5" align="center">Our recommended sources right now:</Typography>
-          <Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-            <a href="https://www.ft.com/"><img src={FTLogo} alt="Financial Times" style={{ width: "80px" }} /></a>
-            <a href="https://www.euronews.com/"><img src={ENLogo} alt="Euronews" style={{ width: "150px" }} /></a>
-            <a href="https://energypost.eu/"><img src={EPLogo} alt="Energy Post" style={{ width: "150px" }} /></a>
-          </Box>
+      </Grid>
+      <Grid item xs={12} lg={4} sx={{ minHeight: "85%" }}>
+        <Paper
+          elevation={10}
+          sx={{
+            p: 5,
+            borderRadius: 10,
+          }}
+        >
+          <iframe
+            title="twitterfeed"
+            src="https://feed.mikle.com/widget/v2/158266/?preloader-text=Loading"
+            height="674px"
+            width="100%"
+            class="fw-iframe"
+            scrolling="no"
+            frameborder="0"
+          ></iframe>
         </Paper>
-      </Box>
-      <Paper
-        elevation={10}
-        sx={{ p: 4, borderRadius: 10, width: "40%", height: "85%" }}
-      >
-        <iframe
-          title="twitterfeed"
-          src="https://feed.mikle.com/widget/v2/158266/?preloader-text=Loading"
-          height="674px"
-          width="100%"
-          class="fw-iframe"
-          scrolling="no"
-          frameborder="0"
-        ></iframe>
-      </Paper>
-    </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Box sx={{height:"32px"}}></Box>
+      </Grid>
+    </Grid>
   );
 }
 
